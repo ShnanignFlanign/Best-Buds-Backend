a93ea4f6a9df942c09ed9e688b13ed28da73ecb8
ShnanignFlanign <seanpflan@live.com>
2022-09-28 16:50:05 -0700

    switched server.js order of things

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-/* == External Modules == */
-const express = require('express')
-const session = require('express-session')
-
+//.env config
 require('dotenv').config();
 
+/* == External Modules == */
+const express = require('express');
+
+/* == Internal Modules == */
+const routes = require('./routes');
 //import cors
-const cors = require('cors')
+const cors = require('cors');
+
+//session
+const session = require('express-session');
+const SESSION_SECRET = process.env.SESSION_SECRET
+
+/* == Port == */
+const PORT = process.env.PORT || 3003;
+
+/* == Express Instance == */
+const app = express();
+
+/* == DB connection == */
+require('./config/db.connection')
+
+
+
 
 //whitelist is an array of development url and deployment url
 const whitelist = ['http://localhost:3003', `${process.env.FRONTEND_URL}`]
-
-const SESSION_SECRET = process.env.SESSION_SECRET
 
 //
 const corsOptions = {
@@ -24,37 +40,24 @@ const corsOptions = {
   credentials:true
 }
 
-/* == Internal Modules == */
-const routes = require('./routes')
-
-/* == Express Instance == */
-const app = express()
-
-/* == Port == */
-const PORT = process.env.PORT || 3003;
-
-
-
-/* == DB connection == */
-require('./config/db.connection')
-
-/* == Middleware == */
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(cors(corsOptions));
 
 /* == Session Secret == */
 app.use(session({
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false
-}))
+}));
+
+/* == Middleware == */
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 /* == Routes == */
 // 1 - this is calling the router folder 
-app.use('/plants', routes.plants)
-app.use('/users', routes.users)
+app.use('/plants', routes.plants);
+app.use('/users', routes.users);
 
 app.listen(PORT, () => {
   console.log('🎉🎊', 'celebrations happening on port', PORT, '🎉🎊',)
-})
+});
